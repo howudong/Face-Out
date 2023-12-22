c2d82c1b551d3b88fbd5e15390d516bf685093ef
howudong <tjdvy963@naver.com>
2023-12-22 23:55:31 +0900

    feat : Aws Kurento 서버에 연결하는 KurentoHandler와 KurentoConfig 기능 구현

--- src/main/java/focusing/FocusingApplication.java
+++ src/main/java/focusing/FocusingApplication.java
@@ -1,13 +1,11 @@
-package springboot.focusing;
+package focusing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FocusingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FocusingApplication.class, args);
 	}
-
 }
---
+++ src/main/java/focusing/config/KurentoConfig.java
@@ -0,0 +1,54 @@
+package focusing.config;
+
+import focusing.controller.handler.KurentoHandler;
+import org.kurento.client.KurentoClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+import java.util.Objects;
+
+@Configuration
+@EnableWebSocket
+public class KurentoConfig implements WebSocketConfigurer {
+    // kms.url 를 application.properties 에 저장 후 사용
+    @Value("${kms.url}")
+    private String kmsUrl;
+
+    @Bean
+    public KurentoHandler createKurentoHandler() {
+        return new KurentoHandler();
+    }
+
+    // Kurento Media Server 를 사용하기 위한 Bean 설정
+    // 환경변수가 들어오면 환경변수를 KMS_URL 로 설정 or
+    // 환경변수에 아무것도 안들어오면 application.properties 에 등록된 kms.url 을 가져와서 사용함
+    @Bean
+    public KurentoClient createKurentoClient() {
+        String envKmsUrl = System.getenv("KMS_URL");
+        if (Objects.isNull(envKmsUrl) || envKmsUrl.isEmpty()) {
+            return KurentoClient.create(kmsUrl);
+        }
+        return KurentoClient.create(envKmsUrl);
+    }
+
+    // 웹 소켓에서 rtc 통신을 위한 최대 텍스트 버퍼와 바이너리 버퍼 사이즈를 설정
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(32768);
+        container.setMaxBinaryMessageBufferSize(32768);
+        return container;
+    }
+
+    // signal 로 요청이 왔을 때 아래의 WebSockerHandler 가 동작하도록 registry 에 설정
+    // 요청은 클라이언트 접속, close, 메시지 발송 등에 대해 특정 메서드를 호출한다
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(createKurentoHandler(), "/signal").setAllowedOrigins("*");
+    }
+}
