9a639d1560719f1b7268d36c2386dc5f2fc12273
howudong <tjdvy963@naver.com>
2023-12-22 16:09:15 +0900

    docs : springboot Project 생성

---
+++ src/main/java/springboot/focusing/FocusingApplication.java
@@ -0,0 +1,13 @@
+package springboot.focusing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusingApplication.class, args);
+	}
+
+}
