c0a1ba20ee9541bd0ff81a93e487dba8e8a41f8d
howudong <tjdvy963@naver.com>
2024-01-11 16:19:26 +0900

    feat : @Component 어노테이션 추가

--- src/main/java/springboot/focusing/handler/KurentoHandler.java
+++ src/main/java/springboot/focusing/handler/KurentoHandler.java
@@ -1,8 +1,10 @@
 package springboot.focusing.handler;
 
 import com.google.gson.JsonObject;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
+@Component
 public interface KurentoHandler {
     void process(WebSocketSession session, JsonObject jsonMessage);
 
