49e8ad61709b8d2939e657af3373d9cae9ade426
howudong <tjdvy963@naver.com>
2024-01-13 22:32:36 +0900

    feat : add Log

--- src/main/java/springboot/focusing/handler/kurento/ReceiveVideoHandler.java
+++ src/main/java/springboot/focusing/handler/kurento/ReceiveVideoHandler.java
@@ -1,6 +1,7 @@
 package springboot.focusing.handler.kurento;
 
 import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.WebSocketSession;
 import springboot.focusing.domain.UserSession;
 import springboot.focusing.handler.KurentoHandler;
@@ -8,6 +9,7 @@ import springboot.focusing.service.UserRegistry;
 
 import java.io.IOException;
 
+@Slf4j
 public class ReceiveVideoHandler implements KurentoHandler {
 
     @Override
@@ -15,6 +17,7 @@ public class ReceiveVideoHandler implements KurentoHandler {
         final String senderName = jsonMessage.get("sender").getAsString();
         final UserSession sender = registry.findByName(senderName);
         final String sdpOffer = jsonMessage.get("sdpOffer").getAsString();
+        log.info("ReceiveVideoHandler : {}", senderName, sdpOffer);
 
         UserSession receiver = registry.findBySession(session);
         receiver.receiveVideoFrom(sender, sdpOffer);
