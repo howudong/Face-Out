026289bdd1c8c428abc664577eadf59ff778964c
howudong <tjdvy963@naver.com>
2024-01-31 18:30:27 +0900

    rename: SpringBootProject 관련 파일 및 폴더를 모두 /backend로 옮김

--- backend/src/main/java/springboot/focusing/config/KurentoConfig.java
+++ backend/src/main/java/springboot/focusing/config/KurentoConfig.java
@@ -70,7 +70,7 @@ public class KurentoConfig implements WebSocketConfigurer {
         return container;
     }
 
-    // signal 로 요청이 왔을 때 아래의 WebSockerHandler 가 동작하도록 registry 에 설정
+    // signal 로 요청이 왔을 때 아래의 WebSocketHandler 가 동작하도록 registry 에 설정
     // 요청은 클라이언트 접속, close, 메시지 발송 등에 대해 특정 메서드를 호출한다
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
