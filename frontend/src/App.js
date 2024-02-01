a8fa232b78f7406e66083636dbc1a6b6517e792e
YOON-CC <whdbscks77@gmail.com>
2024-02-01 20:48:20 +0900

    feat(App.js): 서버에서 받은 parsedMessage.id에 따른 case 구문 구분

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,6 +25,39 @@ function App() {
 
   useEffect(() => {
     ws.current = new WebSocket(WEBSOCKET_URL);
+    ws.current.onopen = function () {
+      console.log('WebSocket connection opened.');
+    };
+    ws.current.onmessage = function (message) {
+      var parsedMessage = JSON.parse(message.data);
+      console.info('Received message: ' + message.data);
+
+      switch (parsedMessage.id) {
+        case 'existingParticipants':
+          onExistingParticipants(parsedMessage);
+          break;
+        case 'newParticipantArrived':
+          onNewParticipant(parsedMessage);
+          break;
+        case 'receiveVideoAnswer':
+          receiveVideoResponse(parsedMessage);
+          break;
+        case 'iceCandidate':
+          participants[parsedMessage.name].rtcPeer.addIceCandidate(parsedMessage.candidate, function (error) {
+            if (error) {
+              console.error("Error adding candidate: " + error);
+              return;
+            }
+          });
+          break;
+        case 'participantExit':
+          onParticipantLeft(parsedMessage); 
+          break;
+        default:
+          console.error('Unrecognized message', parsedMessage);
+      }
+    };
+
   }, []);
 
   return (
